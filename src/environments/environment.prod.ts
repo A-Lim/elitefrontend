2b05b7750fa6516b62ee6d217dc5757266a16cee
Alexius Lim <alexiuslim1994@gmail.com>
2022-01-11 17:13:55 +0800

    feat: update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   name: 'Elite',
   apiUrl: 'http://192.168.1.252:81',
   apiVersion: 'v1',
-  dateFormat: 'dd/MM/yyyy',
+  dateFormat: 'dd/MM/yy',
   dateTimeFormat: 'dd/MM/yy h:mm a',
 };
